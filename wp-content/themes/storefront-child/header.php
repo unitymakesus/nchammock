805ae66f58c9c0e7949e60fd203c6dc1c69c2737
lexinamer <lexinamer@gmail.com>
2018-07-09 14:10:25 -0400

    Google Analytics add

---
+++ wp-content/themes/storefront-child/header.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package storefront
+ */
+
+?><!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+
+<?php if(!is_user_logged_in()): ?>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NTWBS5T');</script>
+<!-- End Google Tag Manager -->
+<?php endif; ?>
+
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<?php if(!is_user_logged_in()): ?>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NTWBS5T"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+<?php endif; ?>
+
+
+<?php do_action( 'storefront_before_site' ); ?>
+
+<div id="page" class="hfeed site">
+	<?php do_action( 'storefront_before_header' ); ?>
+
+	<header id="masthead" class="site-header" role="banner" style="<?php storefront_header_styles(); ?>">
+
+		<?php
+		/**
+		 * Functions hooked into storefront_header action
+		 *
+		 * @hooked storefront_header_container                 - 0
+		 * @hooked storefront_skip_links                       - 5
+		 * @hooked storefront_social_icons                     - 10
+		 * @hooked storefront_site_branding                    - 20
+		 * @hooked storefront_secondary_navigation             - 30
+		 * @hooked storefront_product_search                   - 40
+		 * @hooked storefront_header_container_close           - 41
+		 * @hooked storefront_primary_navigation_wrapper       - 42
+		 * @hooked storefront_primary_navigation               - 50
+		 * @hooked storefront_header_cart                      - 60
+		 * @hooked storefront_primary_navigation_wrapper_close - 68
+		 */
+		do_action( 'storefront_header' ); ?>
+
+	</header><!-- #masthead -->
+
+	<?php
+	/**
+	 * Functions hooked in to storefront_before_content
+	 *
+	 * @hooked storefront_header_widget_region - 10
+	 * @hooked woocommerce_breadcrumb - 10
+	 */
+	do_action( 'storefront_before_content' ); ?>
+
+	<div id="content" class="site-content" tabindex="-1">
+		<div class="col-full">
+
+		<?php
+		do_action( 'storefront_content_top' );

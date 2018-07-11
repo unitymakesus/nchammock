4c38bd282b0e8abef8dfc46b5167b4c4568439e9
lexinamer <lexinamer@gmail.com>
2018-07-11 14:22:43 -0400

    removing what is paypal

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -123,6 +123,15 @@ unset($states['US'][$no_state]);
 return $states;
 }
 
+// Remove What is Paypal
+add_filter( 'woocommerce_gateway_icon', 'bbloomer_remove_what_is_paypal', 10, 2 );
+function bbloomer_remove_what_is_paypal( $icon_html, $gateway_id ) {
+  if( 'paypal' == $gateway_id ) {
+    $icon_html = '<img src="https://www.paypalobjects.com/webstatic/mktg/logo/AM_mc_vs_dc_ae.jpg" alt="PayPal Acceptance Mark">';
+  }
+  return $icon_html;
+}
+
 // Footer Authorized Retailers Shortcode
 function auth_retailers_shortcode() { ob_start(); ?>
 
